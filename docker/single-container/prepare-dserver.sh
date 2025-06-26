deed8267bb8e651c58fd33f8371a13a459cab931
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2025-06-26 14:58:32 +0200

    MAINT: do not create test datasets

--- docker/single-container/prepare-dserver.sh
+++ docker/single-container/prepare-dserver.sh
@@ -9,65 +9,9 @@ echo "-> Creating keys..."
 openssl genrsa -out /keys/jwt_key 2048
 openssl rsa -in /keys/jwt_key -pubout -outform PEM -out /keys/jwt_key.pub
 
-# echo "-> Cretate databases"
-# out=$(flask db init 2>&1)
-# if [ $? -ne 0 ]; then
-#   echo $out | grep -E 'Error: Directory migrations already exists and is not empty' > /dev/null 2>&1
-#   if [ $? -ne 0 ]; then
-#     echo $out >2
-#     exit 1
-#   fi
-# fi
-
-# python3 -m pip install -e .
-
-# echo "-> Listing dtool datasets"
-# dtool ls s3://test-bucket
-
 # Define the local data directory
 DATA_DIR="/tmp/data"
 mkdir -p ${DATA_DIR}
-
-# Add datasets if there are less than two in the system
-if [ $(dtool ls "file://$DATA_DIR" | wc -l) -lt "4" ]; then
-    cd /tmp
-
-    echo "-> Creating test dataset 1"
-    rm -rf test_dataset_1
-    dtool create test_dataset_1
-    echo <<EOF > test_dataset_1/README.yml
-    ---
-    key1: "value1"
-    key2: 2.3
-EOF
-    curl -k https://creativecommons.org/publicdomain/zero/1.0/legalcode.txt > test_dataset_1/data/test_data.txt
-    dtool freeze test_dataset_1
-    dtool cp test_dataset_1 "file://$HOSTNAME$DATA_DIR"
-    rm -rf test_dataset_1
-
-    echo "-> Creating test dataset 2"
-    rm -rf test_dataset_2
-    dtool create test_dataset_2
-    echo <<EOF > test_dataset_2/README.yml
-    ---
-    key1: "value3"
-    key2: 42
-EOF
-    curl -k https://creativecommons.org/licenses/by-nc/4.0/legalcode.txt > test_dataset_2/data/i_am_a_text_file.txt
-    dtool freeze test_dataset_2
-    dtool cp test_dataset_2 "file://$HOSTNAME$DATA_DIR"
-    rm -rf test_dataset_2
-
-    echo "-> Listing dtool datasets"
-    dtool ls "file://$HOSTNAME$DATA_DIR"
-
-    cd /app
-fi
-
-if [ ! -d migrations ]; then
-    echo "-> Initialize database..."
-    flask db init
-fi
 
 echo "-> Migrating database..."
 flask db migrate
